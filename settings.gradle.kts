3eb4384af5c28459735f04d3fc6c39e32c42e99c
BobbyG4 <grove.bob@gmail.com>
2026-08-17 12:18:52 +0900

    chore: version bump - jackson 22.2.1 | gradle 9.7.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ dependencyResolutionManagement {
             version("kotlin", "2.4.0")
             version("ktlint", "12.2.0")
             version("jnats", "2.25.3")
-            version("jackson", "2.21.5")
+            version("jackson", "2.22.1")
             version("coroutines", "1.11.0")
             version("logback", "1.5.37")
             plugin("kotlinJvm", "org.jetbrains.kotlin.jvm").versionRef("kotlin")
